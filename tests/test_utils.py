1ac32b5474b8804c39de050f4f134495cbd22774
Edward Qian <edward.qian@grafana.com>
2023-08-19 23:29:42 -0400

    updated test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,24 +6,20 @@ from openai.error import APIError
 
 
 def test_retry_decorator():
-
-    @max_retries(3)
-    def normal_exception():
-        raise Exception("test")
-
     retry_mock = Mock()
-
     retry_mock.side_effect = [APIError("test"), "success"]
 
-    @max_retries(3, exceptions=[APIError])
+    ## Mismatch retry exception
+    @max_retries(1, exceptions=(ValueError))
+    def normal_exception():
+        return retry_mock()
+
+    ## Matching retry exception
+    @max_retries(1, exceptions=(APIError))
     def openai_exception():
-        result = retry_mock()
-        if isinstance(result, str):
-            return result
-        else:
-            raise result
+        return retry_mock()
     
-    with pytest.raises(Exception):
+    with pytest.raises(APIError):
         normal_exception()
 
     assert openai_exception() == "success"
